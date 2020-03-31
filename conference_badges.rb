abaf2e4fc4a367fbc1d944e84e2fa9e9c8f0ac2f
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-03-31 20:46:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(array_of_names)
   badge_messages=[]
   array_of_names.each do |name|
-    badge_messages<< "Hello, my nmae is #{name}."
+    badge_messages<< "Hello, my name is #{name}."
   end 
   badge_messages 
 end 
