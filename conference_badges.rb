763794810da4458a6b92296c7a324abc1ae67aa8
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-03-31 21:20:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,10 @@ end
 
 def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
-    badge 
+   puts badge 
   end 
   
   assign_rooms(attendees).each do |room|
-    room 
+   puts  room 
   end 
 end 
